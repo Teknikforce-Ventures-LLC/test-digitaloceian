68e6640b853db93d295a944cc72efe101775b341
TUHINSAMANTA-Teknikforce <43666568+TUHINSAMANTA-Teknikforce@users.noreply.github.com>
2020-12-16 14:00:33 +0530

    up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const app= express();
 app.get('/', (req, res)=>{
-    res.end('<h1>Hello</h1>');
+    res.end('<h1>Hello2</h1>');
 });
 
 const PORT= process.env.PORT|| 3002;
